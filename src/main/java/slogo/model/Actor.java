35e6783594ab4befc451f2c33dfbf0780df059d4
Ricky Weerts <rrw24@duke.edu>
2022-02-18 15:57:27 -0500

    Implemented Actor

--- src/main/java/slogo/model/Actor.java
+++ src/main/java/slogo/model/Actor.java
@@ -5,54 +5,75 @@ import javafx.geometry.Point2D;
 /**
  * Represents an actor that can move around the world and has a heading
  */
-public interface Actor {
+public class Actor {
+    private String ID;
+    private Point2D position;
+    private double heading;
+
     /**
      *
      * @return The ID of this actor
      */
-    String getID();
+    String getID() {
+        return ID;
+    }
 
     /**
      * Sets the ID of this actor
-     * @param pos The ID to set it to
+     * @param id The ID to set it to
      */
-    void setID(String id);
+    void setID(String id) {
+        ID = id;
+    }
 
     /**
      *
      * @return The current position of this actor
      */
-    Point2D getPosition();
+    Point2D getPosition() {
+        return position;
+    }
 
     /**
      * Sets the current position of this actor
      * @param pos The position to set this to
      */
-    void setPosition(Point2D pos);
+    void setPosition(Point2D pos) {
+        position = pos;
+    }
 
     /**
      *
      * @return The current heading of this actor
      */
-    double getHeading();
+    double getHeading() {
+        return heading;
+    }
 
     /**
      * Sets the current heading of this actor
      * @param angle The heading to set this to
      */
-    void setHeading(double angle);
+    void setHeading(double angle) {
+        heading = angle;
+    }
 
     /**
      * Moves this actor a certain amount in a specific direction.
      * The actors heading does not necessarily change to point in the direction of motion.
-     * @param angle The angle to move at relative to the actors current heading
+     * @param angle The angle to move at relative to the actors current heading (in radians)
      * @param distance The distance to move
      */
-    void move(double distance, double angle);
+    void move(double distance, double angle) {
+        double moveAngle = heading + angle;
+        position = position.add(distance * Math.sin(moveAngle), distance * Math.cos(moveAngle));
+    }
 
     /**
      * Turns this actor by the specified amount. Essentially just adds the angle specified to the current heading.
-     * @param angle How much to turn the actor
+     * @param angle How much to turn the actor (in radians)
      */
-    void turn(double angle);
+    void turn(double angle) {
+        heading += angle;
+    }
 }
