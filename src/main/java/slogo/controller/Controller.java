ded08b25ba7bc060d35c3b8ec7d06b9c96d9d842
Luke McSween <luke.mcsween@duke.edu>
2022-02-17 22:33:27 -0500

    Created Controller class

---
+++ src/main/java/slogo/controller/Controller.java
@@ -0,0 +1,16 @@
+package slogo.controller;
+
+
+import javafx.stage.Stage;
+
+
+/**
+ * Controller class drives the SLogo program. Initializes and sets up view and model.
+ *
+ * @author Luke McSween
+ */
+
+
+public class Controller {
+
+}
