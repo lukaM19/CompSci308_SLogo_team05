bdc95ba1732c35f6992827d1e4dd891b918c2d89
Vincent Chen <vjc9@duke.edu>
2022-02-20 20:23:47 -0500

    small fixes to setter

--- src/main/java/slogo/command/actorcommand/ActorCommand.java
+++ src/main/java/slogo/command/actorcommand/ActorCommand.java
@@ -11,6 +11,7 @@ import slogo.model.World;
 
 public abstract class ActorCommand extends Command {
   public static final int ACTOR_INDEX = 0;
+  public static final String ACTOR_ID_KEY = "actorID";
 
   protected Actor actor;
 
@@ -31,7 +32,7 @@ public abstract class ActorCommand extends Command {
    * @throws ActorNotFoundException if the actor can't be found in the world
    */
   private Actor getActor(World world) throws ActorNotFoundException, ImpliedParameterException {
-    String actorName = getImpliedParameter(VAR_NAME_KEY);
+    String actorName = getImpliedParameter(ACTOR_ID_KEY);
     try {
       return world.getActor(Integer.parseInt(actorName));
     } catch(NumberFormatException e) {
