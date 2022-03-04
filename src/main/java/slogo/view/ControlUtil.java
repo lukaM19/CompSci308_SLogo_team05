f700a834146e8e000ab51f3eeef61c198930942b
Luka Mdivani <lm378@duke.edu>
2022-03-04 10:33:06 -0500

    can create button with id,added javadoc

--- src/main/java/slogo/view/ControlUtil.java
+++ src/main/java/slogo/view/ControlUtil.java
@@ -6,6 +6,8 @@ import javafx.scene.control.Button;
 
 /**
  * Utility class for making control elements
+ *
+ * @author Luka Mdivani
  */
 public class ControlUtil {
   private ControlUtil(){
@@ -21,7 +23,24 @@ public class ControlUtil {
   public static Button makeButton(String name, EventHandler<ActionEvent> e){
     Button result = new Button(name);
     result.setOnAction(e);
-    result.setId("Canvas"+name);
+
+
+    return result;
+
+  }
+
+  /**
+   * Make a button with a given name and associated event, also set id.
+   *
+   * @param name name of button
+   * @param e the event handler
+   * @param id the tag id of Node.
+   * @return return the new button
+   */
+  public static Button makeButton(String name, EventHandler<ActionEvent> e,String id){
+    Button result = new Button(name);
+    result.setOnAction(e);
+    result.setId(id);
 
     return result;
 
